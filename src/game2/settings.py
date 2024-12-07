f35a01bbf16cd532de115a1d54a60e0d73989b4c
ozennou <ozennouamine02@gmail.com>
2024-12-07 19:49:30 +0100

    fix: add allow host for the health check

--- src/game2/settings.py
+++ src/game2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z&ioz7=a3d^^f2gnn$il-da*xme$dezau+961ultu*ic%krydx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 #ASGI
 
